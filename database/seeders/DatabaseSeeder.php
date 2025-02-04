17392ab446f0ba01735933fa2c77000ae974f8f8
dave de visser <blubdev.llc@gmail.com>
2025-02-04 12:05:51 +0100

    yes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create()->each(function ($user) {
+            $cars = \App\Models\Car::factory(3)->create(['user_id' => $user->id]);
+            $tags = \App\Models\Tag::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            foreach ($cars as $car) {
+                $car->tags()->attach($tags->random(2));
+            }
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    return view('cars.index');
+})->name('index');
 
-Route::middleware('auth')->group(function () {
-    //
+
+
+Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+Route::get('/cars/{car}', [CarController::class, 'show'])->name('cars.show');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/cars/create/step1', [CarController::class, 'createStep1'])->name('cars.create.step1');
+    Route::post('/cars/create/step1', [CarController::class, 'postCreateStep1'])->name('cars.create.step1.post');
+
+    Route::get('/cars/create/step2', [CarController::class, 'createStep2'])->name('cars.create.step2');
+    Route::post('/cars/create/step2', [CarController::class, 'postCreateStep2'])->name('cars.create.step2.post');
+
+    Route::get('/cars/create/step3', [CarController::class, 'createStep3'])->name('cars.create.step3');
+    Route::post('/cars/create/step3', [CarController::class, 'postCreateStep3'])->name('cars.create.step3.post');
 });
 
 require __DIR__.'/auth.php';
+
+
+
